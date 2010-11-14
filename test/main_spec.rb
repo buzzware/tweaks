5c8f426491b4a5552cf2e222adefae4d3a02fba3
buzzware <regos@buzzware.com.au>
2010-11-14 16:45:09 +0800

    cleanup

--- test/main_spec.rb
+++ test/main_spec.rb
@@ -33,7 +33,7 @@ describe "tweaks" do
 		received_config.should == input_config
 	end
 
-	it "should not get executed immediately when given :install_later, but is executed on config" do
+	it "should not get executed immediately when given :install_later, but is executed on config and has merged config" do
 		received_config = nil
 		input_config = {
 			:name => String,
@@ -56,53 +56,8 @@ describe "tweaks" do
 			:age => 52,
 			:a_symbol => :apple
 		}
+		received_config.class.should == TweakConfig
 	end
 
 end
-
-
-
-#class MainTest < Test::Unit::TestCase
-
-#	test "block gets correct config" do
-#		assert true
-		#received_config = nil
-		#input_config = {:food => [:bacon, :bacon_salad, :pizza]}
-    #
-		#Tweak.define_tweak(
-		#	:lunch,
-		#	:install_now,
-		#	input_config
-		#)	do |config,name|
-		#	received_config = config
-		#end
-		#assert_equals(input_config,received_config)
-#	end
-	
-#	test "" do
-#	end
-
-
-	#test "normally enabled then disabled in config doesn't cause it to run"
-	#test "normally enabled then disabled in config doesn't cause it to run"
-	#test "config default and reading works"
-
-
-	#With normally enabled and no config required and do_it always called in the config - how would it get called ?
-	#perhaps need Tweaks.do_default_configured_tweaks()
-  #
-	#or
-  #
-	#Forget normally enabled/disabled : Always normally disabled, in config must Tweaks.configure_tweak(:name,{...}) or at least
-	#Tweaks.configure_tweak(:name)
-  #
-	#Maybe :enabled_now and no config allowed or required
-  #
-	# ! Answer !
-	#
-	#:install_now		# install now with defaults
-	#:install_later	# config and install later in tweaks_config
-
-
-#end
 
